be863b04edae01b65bfa78dba98ad165ea72d64c
Valokoodari <huuhtanen.joona@gmail.com>
2017-11-22 14:01:39 +0200

    Added rotation

--- game.js
+++ game.js
@@ -9,7 +9,7 @@ document.addEventListener("keydown", keyPush);
 /* Basic settings */
 var bgColor = "#2D2D2D";    // Default #2D2D2D
 var gridSize = 40;          // Default 40
-//var speed = 12;             // Default 12
+var speed = 8;             // Default 8
 
 /* Color palette for tetrominos */
 var colors = [
@@ -105,6 +105,15 @@ class Playfield {
         this.grid[block.x][block.y] = color;
     }
 
+    canMove(piece, pos) {
+        for (var i = 0; i < piece.length; i++) {
+            if (piece[i].x + pos.x < 0 || piece[i].x + pos.x > canv.width / gridSize - 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     canMoveDown(piece, pos) {
         for (var i = 0; i < piece.length; i++) {
             if (piece[i].y + pos.y > canv.height / gridSize - 2) {
@@ -151,11 +160,11 @@ class Tetromino {
     }
 
     update() {
-        if (playfield.canMoveDown(this.block, this.pos)) {
+        if (playfield.canMoveDown(this.piece, this.pos)) {
             this.pos.y++;
         } else {
-            for (var i = 0; i < this.block.length; i++) {
-                playfield.append(new Vec2(this.block[i].x + this.pos.x, this.block[i].y + this.pos.y), colors[this.type]);
+            for (var i = 0; i < this.piece.length; i++) {
+                playfield.append(new Vec2(this.piece[i].x + this.pos.x, this.piece[i].y + this.pos.y), colors[this.type]);
             }
             this.reset();
         }
@@ -164,41 +173,65 @@ class Tetromino {
     draw() {
         if (this.pos.y >= 0) {
             ctx.fillStyle = colors[this.type];
-            for (var i = 0; i < this.block.length; i++) {
-                ctx.fillRect((this.pos.x + this.block[i].x) * gridSize, (this.pos.y + this.block[i].y) * gridSize, gridSize, gridSize);
+            for (var i = 0; i < this.piece.length; i++) {
+                ctx.fillRect((this.pos.x + this.piece[i].x) * gridSize, (this.pos.y + this.piece[i].y) * gridSize, gridSize, gridSize);
             }
         }
     }
 
     right() {
-        if (playfield.canMoveRight(this.block, this.pos)) {
+        if (playfield.canMoveRight(this.piece, this.pos)) {
             this.pos.x++;
         }
     }
 
     left() {
-        if (playfield.canMoveLeft(this.block, this.pos)) {
+        if (playfield.canMoveLeft(this.piece, this.pos)) {
             this.pos.x--;
+        }
+    }
+
+    rotate() {
+        // Don't rotate squares
+        if (this.type === 1) {
+            return;
+        }
+
+        // Array to store the possible new block
+        var nPiece = [];
+        
+        for (var i = 0; i < this.piece.length; i++) {
+            nPiece.push(new Vec2(this.piece[i].y * -1, this.piece[i].x));
+        }
+
+        if (playfield.canMove(nPiece, this.pos)) {
+            this.piece = nPiece;
+        }
+    }
+
+    drop() {
+        while (playfield.canMoveDown(this.piece, this.pos)) {
+            this.pos.y++;
         }
     }
 
     reset() {
         this.type = rand(0, 7);
-        this.block = blocks[this.type][0];
-        this.pos = new Vec2(rand(blocks[this.type][1], blocks[this.type][2]), 0);
+        this.piece = blocks[this.type][0];
+        this.pos = new Vec2(rand(blocks[this.type][1], blocks[this.type][2]), -2);
     }
 }
 
 
 /* The hearth of the game */
-function gameLoop() {
+function mainLoop() {
     background.draw();
     ctx.fillStyle = "#008FFF";
     ctx.fillText(a, 10, 10);
     if (a === 0) {
         active.update();
         playfield.update();
-        a = 8;
+        a = speed;
     }
     a--;
     active.draw();
@@ -217,13 +250,13 @@ function keyPush(evt) {
             active.left();
             break;
         case 38:        // Up Arrow
-            // rotate
+            active.rotate()
             break;
         case 39:        // Right Arrow
             active.right();
             break;
         case 40:        // Down Arrow
-            // drop
+            active.drop();
             break;
         case 82:        // R
             // restart
@@ -234,13 +267,13 @@ function keyPush(evt) {
 
 /* Templates for all possible tetrominos */
 var blocks = [
-    [[new Vec2(-1, -1), new Vec2(0, -1), new Vec2(1, -1), new Vec2(2, -1)], 1, 7],  // I
-    [[new Vec2(0, -2), new Vec2(1, -2), new Vec2(0, -1), new Vec2(1, -1)], 0, 8],   // O
-    [[new Vec2(1, -2), new Vec2(0, -1), new Vec2(1, -1), new Vec2(2, -1)], 0, 8],   // T
-    [[new Vec2(1, -2), new Vec2(2, -2), new Vec2(0, -1), new Vec2(1, -1)], 0, 8],   // S
-    [[new Vec2(0, -2), new Vec2(1, -2), new Vec2(1, -1), new Vec2(2, -1)], 0, 8],   // Z
-    [[new Vec2(0, -2), new Vec2(0, -1), new Vec2(1, -1), new Vec2(2, -1)], 0, 8],   // J
-    [[new Vec2(2, -2), new Vec2(0, -1), new Vec2(1, -1), new Vec2(2, -1)], 0, 8]    // L
+    [[new Vec2(-1, 0), new Vec2( 0, 0), new Vec2( 1, 0), new Vec2(2, 0)], 1, 7],    // I (Final)
+    [[new Vec2(-1, 0), new Vec2( 0, 0), new Vec2(-1, 1), new Vec2(0, 1)], 1, 9],    // O
+    [[new Vec2( 0, 0), new Vec2(-1, 1), new Vec2( 0, 1), new Vec2(1, 1)], 1, 9],    // T
+    [[new Vec2( 0, 0), new Vec2( 1, 0), new Vec2(-1, 1), new Vec2(0, 1)], 1, 9],    // S
+    [[new Vec2(-1, 0), new Vec2( 0, 0), new Vec2( 0, 1), new Vec2(1, 1)], 1, 9],    // Z
+    [[new Vec2(-1, 0), new Vec2(-1, 1), new Vec2( 0, 1), new Vec2(1, 1)], 1, 9],    // J
+    [[new Vec2( 1, 0), new Vec2(-1, 1), new Vec2( 0, 1), new Vec2(1, 1)], 1, 9]     // L
 ];
 
 
@@ -249,4 +282,4 @@ var background = new Background(bgColor);
 var playfield = new Playfield();
 var active = new Tetromino();
 var a = 0; 
-setInterval(gameLoop, 1000/60);
+setInterval(mainLoop, 1000/60);
